e463fe8fe94bce4b48560fbf3cbaa6cf9ec7a39d
Marcin Karmelita <mkarmelita@hotmail.com>
2017-04-10 21:55:38 +0200

    Initial Commit

---
+++ TotalCommander/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TotalCommander
+//
+//  Created by Marcin Karmelita on 10/04/17.
+//  Copyright © 2017 Marcin Karmelita. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TotalCommander/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TotalCommander
+//
+//  Created by Marcin Karmelita on 10/04/17.
+//  Copyright © 2017 Marcin Karmelita. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
